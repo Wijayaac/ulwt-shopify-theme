210d69527f29cffb843e8b16068e240c66330223
Wijayaac <wijayaac25@gmail.com>
2023-02-27 07:59:10 +0800

    Add : edge text media section

---
+++ assets/edge-text-media.js
@@ -0,0 +1,22 @@
+class EdgeSection extends HTMLElement {
+  constructor() {
+    super();
+    this.containerElement = document.body.querySelector(".page-width");
+    this.edgeSection = this.querySelector(".edge-tm");
+
+    this.setSpace();
+
+    const resizeObserver = new ResizeObserver((entries) => this.setSpace());
+    resizeObserver.observe(this);
+  }
+  setSpace() {
+    console.log("here", this.containerElement);
+    let space =
+      this.containerElement.getBoundingClientRect().left ||
+      this.containerElement.offsetLeft;
+
+    this.style.setProperty("--spacing", `${space + 30}px`);
+  }
+}
+
+customElements.define("edge-section", EdgeSection);
