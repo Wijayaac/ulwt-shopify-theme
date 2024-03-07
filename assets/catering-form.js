493f9d39be7fefbe139fe878a2eb8411f045089e
Wijayaac <wijayaac25@gmail.com>
2024-03-07 15:04:44 +0700

    Add : form validation for required fields

---
+++ assets/catering-form.js
@@ -0,0 +1,30 @@
+const initializeCateringForm = () => {
+  const form = document.querySelectorAll(".upsx-cf-form");
+
+  if (!form[0]) {
+    return;
+  }
+
+  const fields = Array.from(document.querySelectorAll(".upsx-cf-form [aria-required='true']"));
+  if (!fields[0]) {
+    return;
+  }
+
+  checkOnBlurFields(fields);
+};
+
+function checkOnBlurFields(fields) {
+  fields.forEach((field) => {
+    field.addEventListener("blur", () => {
+      if (field.value === "") {
+        field.parentNode.classList.add("error");
+      } else {
+        field.parentNode.classList.remove("error");
+      }
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  initializeCateringForm();
+});
